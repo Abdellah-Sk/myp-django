5dd78a1a3dc4ba1c29382a574fddf8114f8eeff7
Abdellah-Sk <skoundriabdellah@gmail.com>
2020-09-15 20:41:30 +0200

    modif models projects and techno

--- src/projects/models.py
+++ src/projects/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Technology(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+
+class Project(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=255)
+    sub_title = models.CharField(max_length=255)
+    excerpt = models.TextField(null=True)
+    description = models.TextField()
+    # image = models.CharField(max_length=255)
+    images = models.ImageField(upload_to='img')
+    repo_url = models.CharField(max_length=255)
+    website_url = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('date published')
+    technology = models.ManyToManyField(Technology)
+
+    def __str__(self):
+        return self.title
+
+
