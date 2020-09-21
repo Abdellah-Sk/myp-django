9e0c93cb859f5511faacffb85accfc90e5f92f8a
Abdellah-Sk <skoundriabdellah@gmail.com>
2020-09-21 14:27:20 +0200

    url for img

--- src/PortfolioDjango/urls.py
+++ src/PortfolioDjango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from projects.views import index
 from projects.views import projects
 from projects.views import contact
@@ -25,4 +28,4 @@ urlpatterns = [
     path('projects/', projects),
     path('contact/', contact),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
