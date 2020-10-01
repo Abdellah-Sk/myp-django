373dc09905aa14214f2dfb32cf5d45f39f598355
Abdellah-Sk <skoundriabdellah@gmail.com>
2020-10-01 16:35:34 +0200

    display contact model in django admin

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 from .models import Project
 from .models import Technology
+from .models import Contact
 
 admin.site.register(Project)
 admin.site.register(Technology)
+admin.site.register(Contact)
+
 
 
