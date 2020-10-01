cd446f316df532a7cd755b05d17af078b9b2ff25
Abdellah-Sk <skoundriabdellah@gmail.com>
2020-10-01 16:30:03 +0200

    create a model for message contact

--- src/projects/models.py
+++ src/projects/models.py
@@ -26,3 +26,11 @@ class Project(models.Model):
         return self.title
 
 
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
