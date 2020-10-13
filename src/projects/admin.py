74f6b1b8feeeed10e230530675fcefff03c2a542
Abdellah-Sk <skoundriabdellah@gmail.com>
2020-10-13 16:41:58 +0200

    print profil model in admin panel

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from .models import Project
 from .models import Technology
 from .models import Contact
+from .models import Profile
 
 admin.site.register(Project)
 admin.site.register(Technology)
 admin.site.register(Contact)
+admin.site.register(Profile)
 
 
 
