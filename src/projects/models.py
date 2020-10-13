0dffe535699c737f7f07f4265e92955ed0fb78a8
Abdellah-Sk <skoundriabdellah@gmail.com>
2020-10-13 16:39:19 +0200

    update user model

--- src/projects/models.py
+++ src/projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Technology(models.Model):
@@ -33,4 +34,16 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=50, blank=True)
+    linkedin = models.CharField(max_length=100, blank=True)
+    github = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+
 
